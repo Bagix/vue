22388c53dfa0ddf67ad7d22c3f11a113833424d6
Mateusz Boruszczak <borsuk1253@gmail.com>
2019-05-10 19:53:30 +0200

    some vue introduction

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var app = new Vue({
 var app2 = new Vue({
   el: '#app-2',
   data: {
-    message: 'Załadowałeś tą stronę o  ' + new Date().toLocaleString()
+    message: 'Załadowałeś tę stronę o  ' + new Date().toLocaleString()
   }
 })
 
@@ -46,5 +46,23 @@ var app6 = new Vue({
   el: '#app-6',
   data: {
     message: 'No spoko wiadmość'
+  }
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    shopList: [
+      {id: 0, text: 'Bread'},
+      {id: 1, text: 'Apples'},
+      {id: 2, text: 'Beer'},
+    ],
+    newItem: '',
+  },
+  methods: {
+    addItem: function() {
+      newId = this.shopList.length;
+      this.shopList.push({id: newId, text: this.newItem});
+    }
   }
 })
